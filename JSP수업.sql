c981ee8c373a30711c53fed38395987dc7afc445
peter <gudqo1995@naver.com>
2020-08-18 22:39:27 +0900

    jsp 수업

---
+++ JSP수업.sql
@@ -0,0 +1,22 @@
+create table PERSON (
+NUM number primary key not null,
+NAME varchar2(10) not null,
+BIRTH date,
+TEL varchar2(20) not null
+);
+insert into PERSON
+    values(1, '홍길동', '18-08-27', '010-1111-2222');
+insert into PERSON
+    values(2, '이순신', sysdate, '010-2222-3333');
+insert into PERSON
+    values(3, '강감찬', '20-04-13', '010-3333-4444');
+insert into PERSON
+    values(4, '임꺽정', sysdate, '011-5555-6666');
+    
+select * from PERSON;
+select NUM, NAME from PERSON;
+select * from PERSON where NAME='임꺽정';
+select * from PERSON where NAME like '이%' and NUM=2;
+select * from PERSON where NAME like '%감%';
+
+commit;
